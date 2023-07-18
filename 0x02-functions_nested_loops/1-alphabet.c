d52f34f0dab35c23a552ff8371e943b22ba7e66a
osos0220 <42010654.osos@gmail.com>
2023-07-18 16:21:58 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,20 @@
-#include"main.h"
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
 void print_alphabet(void)
 {
-	int ch;
-	for (ch = 'a'; ch <= 'z';ch++)
-		_putchar(ch);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
 	_putchar('\n');
-}	
+
+}
